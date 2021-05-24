b3b97a72c5585f8faa8a37253e69912200f1d87d
Rain Wang <3045465496@qq.com>
2021-05-24 11:09:33 +0800

    design the SyntaxTree struct

---
+++ ast.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"ast.h"
+#include<stdarg.h>
+#include<stdlib.h>
+#include<string.h>
+extern int yylineno;
+
+SyntaxTree* create(char* name, char* s, int is_nonterminal)
+{
+
+}
+
+void add_ast_node(int child_num, SyntaxTree* parent, ...)
+{
+
+}
+
+void print_ast(SyntaxTree* root, int count)
+{
+    
+}
