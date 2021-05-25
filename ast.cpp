1ae426b2ba86a842e8038a8e23122ac883f68899
RainWang6188 <3045465496@qq.com>
2021-05-25 16:05:15 +0800

    finishes lexical analysis section

--- ast.cpp
+++ ast.cpp
@@ -1,21 +1,62 @@
 #include<stdio.h>
-#include"ast.h"
 #include<stdarg.h>
 #include<stdlib.h>
 #include<string.h>
+#include"ast.h"
 extern int yylineno;
 
-SyntaxTree* create(char* name, char* s, bool is_nonterminal)
+SyntaxTree* create(char* name, char* s, int is_nonterminal)
 {
-
+    SyntaxTree* tree = (SyntaxTree*)malloc(sizeof(struct SYNTAXTREE));
+    
+    strcpy(tree->name, name);
+    strcpy(tree->text, s);
+    tree->line_no = yylineno;
+    tree->child_num = 0;
+    tree->is_nonterminal = is_nonterminal;
+    for(int i = 0; i < MAX_CHILD_NUM; i++)
+        tree->child_ast[i] = NULL;
+    
+    return tree;
 }
 
 void add_ast_node(int child_num, SyntaxTree* parent, ...)
 {
-
+    va_list p;
+    va_start(p,parent);
+    
+    for(int i = 0; i < child_num; i++){
+        SyntaxTree* node; 
+        node = va_arg(p, SyntaxTree*);
+        parent->child_ast[i] = node;
+    }
+    parent->child_num=child_num;
+    parent->line_no = parent->child_ast[0]->line_no;
+    va_end(p);
 }
 
 void print_ast(SyntaxTree* root, int count)
 {
-
+    if(root){
+        for(int i = 0; i < count; i++)
+            printf("  ");
+        printf("%s",root->name);
+        
+        if(!root->is_nonterminal){
+            if(strcmp(root->name,"ID")==0)
+                printf(": %s",root->text);
+            else if(strcmp(root->name,"TYPE")==0)
+                printf(": %s",root->text);
+            else if(strcmp(root->name,"INT")==0)
+                printf(": %d",atoi(root->text));   
+            else if(strcmp(root->name,"FLOAT")==0)
+                printf(": %f",atof(root->text));
+        }
+        else
+            printf(" (%d)",root->line_no);
+        printf("\n");
+        //printf("%d\n",root->childnum);
+        for(int i = 0; i < root->child_num; i++)
+            print_ast(root->child_ast[i],count+1);
+    }
 }
--- parse.y
+++ parse.y
@@ -1,3 +1,58 @@
+
 %{
-        
+    #include<stdio.h>
+    #include "lex.yy.c"
+    #include"ast.h"
+    void yyerror(char* msg);
+    extern SyntacxTree* ROOT;
 %}
+
+/*declared types*/
+%union
+{
+    struct SYNTAXTREE* type_tree;
+}
+%token <type_tree> INT
+%token <type_tree> FLOAT
+%token <type_tree> ID
+%token <type_tree> SEMI
+%token <type_tree> COMMA
+%token <type_tree> ASSIGNOP
+%token <type_tree> RELOP
+%token <type_tree> PLUS MINUS STAR DIV
+%token <type_tree> AND OR 
+%token <type_tree> NOT
+%token <type_tree> DOT
+%token <type_tree> TYPE
+%token <type_tree> LP RP LB RB LC RC
+%token <type_tree> STRUCT
+%token <type_tree> RETURN
+%token <type_tree> IF
+%token <type_tree> ELSE
+%token <type_tree> WHILE
+%token <type_tree> LCOM
+%token <type_tree> RCOM
+
+
+%nonassoc LOWER_THAN_ELSE
+%nonassoc ELSE
+
+%right ASSIGNOP
+%left OR
+%left AND
+%left RELOP
+%left PLUS MINUS
+%left STAR DIV
+%right NOT
+%left LP RP LB RB DOT
+
+%type <type_tree> Program ExtDefList ExtDef ExtDecList Specifier StructSpecifier OptTag Tag
+%type <type_tree> VarDec FunDec VarList ParamDec CompSt StmtList Stmt DefList Def DecList Dec Exp Args
+
+%%
+
+%%
+void yyerror(char* msg)
+{
+        ;
+}
