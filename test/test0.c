96a93b17ea9e1b30b751e27c5c1ed6dad27ac9b1
Rain Wang <3045465496@qq.com>
2021-06-29 15:56:31 +0800

    [test] update test cases

--- test/test0.c
+++ test/test0.c
@@ -1,13 +1,7 @@
-int d;
-int test(int a) {
-    int b;
-    int c[10];
-    d = 2;
-    return b*c[10];
-}
+int main()
+{
+   int i;
+    i = 0;
 
-int main() {
-    int array[10];
-    array[0] = 2;
-    test(array[0]);
+    return 0;
 }
